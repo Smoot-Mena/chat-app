13e2cad9388eb5e2113b1e67f5765eeeb067e0f9
Smoot-Mena <Msmoot810@gmail.com>
2024-02-01 10:29:35 -0500

    Updates Vite config to localhost

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // "/auth": "http://localhost:3216",
-      // "/api": "http://localhost:3216"
-      "/auth": "https://smoots-fake-aim-chat-app.onrender.com",
-      "/api": "https://smoots-fake-aim-chat-app.onrender.com"
+      "/auth": "http://localhost:3216",
+      "/api": "http://localhost:3216"
+      // "/auth": "https://smoots-fake-aim-chat-app.onrender.com",
+      // "/api": "https://smoots-fake-aim-chat-app.onrender.com"
     }
   }
 })
